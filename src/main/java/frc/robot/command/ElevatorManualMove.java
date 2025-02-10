4aaf14b5ebcd27f674c2c5bf1cdda111973f885d
sburnham3 <sburnham3@gmail.com>
2025-02-10 06:54:59 -0500

    add back some declarations

--- src/main/java/frc/robot/command/ElevatorManualMove.java
+++ src/main/java/frc/robot/command/ElevatorManualMove.java
@@ -16,7 +16,7 @@ public class ElevatorManualMove extends Command {
     
     this.elevator=elevator;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(this.elevator);
+    // addRequirements(this.elevator);
   }
 
   // Called when the command is initially scheduled.
